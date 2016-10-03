3ae1bcd21e901b82d730a886b13f39bcc37462dd
Ani Sinanaj <ani.sinanaj@caffeina.com>
2016-10-03 17:59:35 +0200

    Initial Commit

---
+++ netControl/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  netControl
+//
+//  Created by Ani Sinanaj on 03/10/16.
+//  Copyright © 2016 Ani Sinanaj. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
